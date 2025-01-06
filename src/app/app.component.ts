ceb227e318cde080f8ac7483709dca40d2f4cc75
Natalia Milani <natalia.milani@hotmail.com>
2025-01-06 00:47:24 -0300

    feat: Aula de criação de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
+import {MeuSegundoComponent} from './meu-segundo/meu-segundo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeuPrimeiroComponent, MeuSegundoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
